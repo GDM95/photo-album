cdcf3827dc1adecfff4b8c7a8f3700d338ff560b
Eric S Kim <esk57@rutgers.edu>
2019-04-10 02:19:44 -0400

    Renamed PixelData.java to ImageData.java. More complete implementations of ImageData and Pic models.

--- src/model/ImageData.java
+++ src/model/ImageData.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
  * @author User
  *
  */
-public class PixelData implements Serializable {
+public class ImageData implements Serializable {
 	
 	/**
 	 * explicit serialVersionUID for compiler consistency
@@ -24,23 +24,29 @@ public class PixelData implements Serializable {
 	 * The width and height dimensions of the image
 	 */
 	private int width, height;
+	/**
+	 * An array of integers representing each pixel in the image
+	 */
 	private int[] pixels;
 	
 	/**
-	 * Private constructor
+	 * Default constructor
 	 */
-	public PixelData() {}
+	public ImageData() {}
 	
 	/**Converts the int[] pixel data into an Image object
 	 * @return		an Image object from the stored pixel data
 	 */
-	public Image writeImage() {
+	public Image getImageFromPixels() {
 		WritableImage image = new WritableImage(width, height);
 		image.getPixelWriter().setPixels(0, 0, width, height, PixelFormat.getIntArgbInstance(), pixels, 0, width);
 		return image;
 	}
 	
-	public void readImage(Image image) {
+	/**Feeds the Image's pixel data into an int[] array.
+	 * @param image		the image to be parsed
+	 */
+	public void setPixelsFromImage(Image image) {
 		width = (int) image.getWidth();
 		height = (int) image.getHeight();
 		PixelReader reader = image.getPixelReader();
@@ -49,19 +55,32 @@ public class PixelData implements Serializable {
 		}
 	}
 	
+	/**Gets the Image's width
+	 * @return the ImageData width field
+	 */
 	public int getWidth() {
 		return width;
 	}
 	
+	/**Gets the Image's height
+	 * @return the ImageData height field
+	 */
 	public int getHeight() {
 		return height;
 	}
 	
+	/**Gets the ImageData pixel array
+	 * @return the ImageData pixel array
+	 */
 	public int[] getPixels() {
 		return pixels;
 	}
 	
-	public boolean equals(PixelData pd) {
+	/**Checks whether two images are equal based on their ImageData
+	 * @param pd	the ImageData being compared
+	 * @return True if they are equal. False otherwise.
+	 */
+	public boolean equals(ImageData pd) {
 		if(width != pd.getWidth()) return false;
 		if(height != pd.getHeight()) return false;
 		for(int i = 0; i < width * height; i++) {
