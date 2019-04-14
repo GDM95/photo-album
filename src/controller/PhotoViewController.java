4b68f902930b1197a1457ca5801598caf1fa99e5
Eric S Kim <esk57@rutgers.edu>
2019-04-13 20:59:10 -0400

    Changes for real this time.

--- src/controller/PhotoViewController.java
+++ src/controller/PhotoViewController.java
@@ -1,6 +1,13 @@
 package controller;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -15,6 +22,8 @@ public class PhotoViewController {
 	
 	@FXML GridPane photogrid;
 	
+	private Album current_album;
+	
 	@FXML
 	public void initialize() {
 		UserList.deserializeUsers();
@@ -22,6 +31,65 @@ public class PhotoViewController {
 		caption.setEditable(false);
 		date.setEditable(false);
 		tags.setEditable(false);
+		
+		current_album = UserList.getCurrentAlbum();
+	}
+	
+	@FXML
+	private void goBack(ActionEvent e) {
+		UserList.serializeUsers();
+		
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("../view/album_view.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();	
+			stage.hide();
+			
+			stage.setScene(scene);
+			stage.show();
+		} catch(IOException error) {
+			error.printStackTrace();
+		}
+	}
+	
+	@FXML
+	private void addPhoto() {
+		
+	}
+	
+	@FXML
+	private void deletePhoto() {
+		
+	}
+	
+	@FXML
+	private void movePhoto() {
+		
+	}
+	
+	@FXML
+	private void copyPhoto() {
+		
+	}
+	
+	@FXML
+	private void addTag() {
+		
+	}
+	
+	@FXML
+	private void removeTag() {
+		
+	}
+	
+	@FXML
+	private void previousPhoto() {
+		
+	}
+	
+	@FXML
+	private void nextPhoto() {
+		
 	}
 	
 	public void start(Stage primaryStage) {
